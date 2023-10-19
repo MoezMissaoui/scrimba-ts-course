5f28e9d65c6228d93b525875b7850c2904dd4093
moezmissaoui <moeiz.missaoui@gmail.com>
2023-10-19 11:01:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# scrimba-ts-course
---
+++ index.ts
@@ -0,0 +1,53 @@
+// Number Types mini-challenge 10 
+// Write a function that will only accept numbers and attend to 
+// all TypeScript weakness flags.
+const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
+
+const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
+const userNameDisplay = document.querySelector('#user') as HTMLElement
+
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+// Solution
+function showReviewTotal (value : number, reviewer : string, isLoyalty : boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total: ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
